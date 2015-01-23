de74a15b5b634260f2a32ac04ce6f232e22d8815
Adam Martinik <a.martinik@gmail.com>
2015-01-23 11:02:26 +0100

    Change config

--- config.rb
+++ config.rb
@@ -46,6 +46,7 @@ activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.build_before = true
   deploy.branch   = 'master'
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
