93919442cc04037bae9ae5c9a28243c2acf8544b
Adam Martinik <a.martinik@gmail.com>
2017-10-02 19:44:44 +0200

    feat: add asset_host

--- config.rb
+++ config.rb
@@ -32,6 +32,8 @@ configure :build do
   # Enable cache buster
   activate :asset_hash
 
+  activate :asset_host, :host => '//adammartinik.cz'
+
   # Use relative URLs
   # activate :relative_assets
 
@@ -40,14 +42,4 @@ configure :build do
   activate :google_analytics do |ga|
     ga.tracking_id = 'UA-54308156-1'
   end
-end
-
-activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  deploy.build_before = true
-  deploy.branch   = 'master'
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
